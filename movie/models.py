8062eb4aa3f7d7de2dffd8b628fbe80244723c27
Rakib013 <mdrakib32661@gmail.com>
2021-11-25 11:54:24 +0600

    update some user related bug

--- movie/models.py
+++ movie/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
 
